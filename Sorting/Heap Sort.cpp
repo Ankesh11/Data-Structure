11ba9c15b37971e6c8000b82239acd2a55ffe077
Ankesh Kumar <31979597+Ankesh11@users.noreply.github.com>
2018-07-25 12:39:13 +0530

    Update Heap Sort.cpp

--- Sorting/Heap Sort.cpp
+++ Sorting/Heap Sort.cpp
@@ -1,54 +1,46 @@
-#include<iostream>
-#include<string.h>
-#include<cstring>
-#define MAX 20
+#include<bits/stdc++.h>
 using namespace std;
-void swap_data(int &a,int &b)
-{
-    int temp = a;
-    a = b;
-    b = temp;
+int size = 0;
+void swap(int &a,int &b){
+	int temp = a;
+	a = b;
+	b = temp;
 }
-void max_heapify(int arr[],int n,int p)
-{
-    int largest=p;
-    int l = 2*p + 1;
-    int r = 2*p + 2;
-    if(l<n && arr[l]>arr[largest])
-        largest=l;
-    if(r<n && arr[r]>arr[largest])
-        largest = r;
-    if(largest!=p){
-        swap_data(arr[p],arr[largest]);
-        max_heapify(arr,n,largest);
-    }
+void max_heapify(int arr[],int i){
+	int leftc = i*2+1;
+	int rightc = i*2+2;
+	int largest = i;
+	if(leftc<size && arr[leftc]>arr[largest])
+		largest = leftc;
+	if(rightc<size && arr[rightc]>arr[largest])
+		largest = rightc;
+	if(largest!=i){
+		swap(arr[i],arr[largest]);
+		max_heapify(arr,largest);
+	}
 }
-void build_heap(int arr[],int n)
-{
-    for(int i=n/2;i>=0;i--){
-        max_heapify(arr,n,i);
-    }
+void build_heap(int *arr){
+	for(int i=(size-1)/2;i>=0;i--)
+		max_heapify(arr,i);
 }
-void heap_sort(int arr[],int n)
-{
-    build_heap(arr,n);
-    for(int i=n-1;i>=1;i--){
-        swap_data(arr[i],arr[0]);
-        max_heapify(arr,i,0);
-    }
+void heap_sort(int arr[], int n){
+	size = n;
+	build_heap(arr);
+	for(int i=size-1;i>=1;i--){
+		swap(arr[0],arr[i]);
+		size--;
+		max_heapify(arr,0);
+	}
 }
-int main()
-{
-    int n;
-    cout<<"Enter the no of elements: ";
-    cin>>n;
-    int arr[n];
-    cout<<"Enter the elements:\n";
-    for(int i=0;i<n;i++)
-        cin>>arr[i];
-    heap_sort(arr,n);
-    cout<<"Sorted array:\n";
-    for(int i=0;i<n;i++)
-        cout<<arr[i]<<" ";
-    return 0;
+int main(){
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+		cin>>arr[i];
+	heap_sort(arr,n);
+	cout<<"Sorted array:\n";
+	for(int i=0;i<n;i++)
+		cout<<arr[i]<<" ";
+	return 0;
 }
