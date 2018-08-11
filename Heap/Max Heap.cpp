57e307ba126580318bea9f9da3114f42fe25f0a4
Ankesh Kumar <31979597+Ankesh11@users.noreply.github.com>
2018-08-11 14:16:53 +0530

    Update Max Heap.cpp

--- Heap/Max Heap.cpp
+++ Heap/Max Heap.cpp
@@ -1,86 +1,90 @@
-#include<iostream>
-#include<string.h>
-#include<cstring>
-#define MAX 20
+#include <bits/stdc++.h>
 using namespace std;
-int N=-1;
-void percolate_up(int heap[],int val)
-{
-    N++;
-    heap[N]=val;
-    int c=N;
-    int p=(N-1)/2;
-    while(heap[p]<heap[c] && c>0){
-        int temp=heap[p];
-        heap[p]=heap[c];
-        heap[c]=temp;
-        c=p;
-        p=(p-1)/2;
-    }
+void swap(int &a,int &b){
+    int temp = a;
+    a = b;
+    b = temp;
 }
-int percolate_down(int heap[])
-{
-    int temp=heap[0];
-    heap[0]=heap[N];
-    N--;
-    int p=0,c1=(2*p)+1,c2=(2*p)+2;
-    if(heap[p]>=heap[c1] && heap[p]>=heap[c2])
-        return temp;
-    else{
-        while((heap[p]<heap[c1] || heap[p]<heap[c2]) && c1<=N){
-            if(heap[c1]>heap[c2]){
-                int temp=heap[c1];
-                heap[c1]=heap[p];
-                heap[p]=temp;
-                p=c1;
-                c1=(2*p)+1;
-                c2=(2*p)+2;
-            }
-            else{
-                int temp=heap[c2];
-                heap[c2]=heap[p];
-                heap[p]=temp;
-                p=c2;
-                c1=(2*p)+1;
-                c2=(2*p)+2;
-            }
+//Max heap
+class max_heap{
+    int *heap;
+    int size;
+    int capacity;
+public:
+    max_heap(int cap){
+        heap = new int[cap];
+        capacity = cap;
+        size=0;
+    }
+    max_heap(int arr[],int n){
+        capacity = n;
+        size = n;
+        heap = arr;
+        build_heap();
+    }
+    void build_heap(){
+        for(int i=(size-1)/2;i>=0;i--)
+            max_heapify(i);
+    }
+    int parent(int i){
+        return (i-1)/2;
+    }
+    int left_child(int i){
+        return 2*i+1;
+    }
+    int right_child(int i){
+        return 2*i+2;
+    }
+    void max_heapify(int i){
+        int l = left_child(i);
+        int r = right_child(i);
+        int largest = i;
+        if(l<size && heap[l]>heap[largest])
+            largest = l;
+        if(r<size && heap[r]>heap[largest])
+            largest = r;
+        if(largest!=i){
+            swap(heap[largest], heap[i]);
+            max_heapify(largest);
         }
     }
-    return temp;
-}
-void display(int heap[])
-{
-    for(int i=0;i<=N;i++)
-        cout<<heap[i]<<"  ";
-}
-int main()
-{
-    int heap[20],val,opt;
-    do{
-        cout<<"\n---MAX HEAP---\n";
-        cout<<"1: Insert\n";
-        cout<<"2: Delete\n";
-        cout<<"3: Display\n";
-        cout<<"4: Exit\n";
-        cout<<"Enter your option: ";
-        cin>>opt;
-        switch(opt)
-        {
-        case 1:
-            cout<<"Enter data to insert: ";
-            cin>>val;
-            percolate_up(heap,val);
-            break;
-        case 2:
-            val=percolate_down(heap);
-            cout<<"Deleted value: "<<val<<endl;
-            break;
-        case 3:
-            display(heap);
-            break;
-        case 4:
-            break;
+    void insert_key(int key){
+        heap[size]=INT_MIN;
+        size++;
+        increase_key(size-1,key);
+    }
+    void increase_key(int i,int new_key){
+        if(heap[i]>new_key)
+            return;
+        heap[i]=new_key;
+        while(i>0 && heap[parent(i)]<heap[i]){
+            swap(heap[parent(i)], heap[i]);
+            i = parent(i);
         }
-    }while(opt!=4);
+    }
+    int extract_max(){
+        if(size==0)
+            return INT_MIN;
+        int max = heap[0];
+        heap[0]=heap[size-1];
+        size--;
+        max_heapify(0);
+        return max;
+    }
+    int get_max(){
+        if(size==0)
+            return INT_MIN;
+        return heap[0];
+    }
+    int get_size(){
+        return size;
+    }
+    void print(){
+        for(int i=0;i<size;i++)
+            cout<<heap[i]<<" ";
+    }
+};
+int main(){
+    //Create max_heap object and use the function here
     return 0;
 }
